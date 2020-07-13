f6165f4ffd2bf18b53e9e0acc98f4ecca1047146
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-13 15:00:19 -0400

    Code for pepp screen test

---
+++ pepscreen.py
@@ -0,0 +1,68 @@
+from Tkinter import *
+import tkFont
+#import RPi.GPIO as GPIO
+import time
+import sys
+#import serial
+import datetime
+#import pyfireconnect
+#from firebase import firebase
+#import urllib
+import json
+import os
+
+#Raspberry Pi set up
+#GPIO.setmode(GPIO.BOARD)
+
+#Motor set up
+#************************
+
+#TK screen set up
+screen = Tk()
+screen.overrideredirect(1)
+screen.geometry('800x480')
+screen.title("Sm^rt Pepp")
+
+#Fonts for screen
+myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
+myFontSmall = tkFont.Font(family = 'Helvetica', size = 12, weight = 'bold')
+myFontMed = tkFont.Font(family = 'Helvetica', size = 20, weight = 'bold')
+myFontLarge = tkFont.Font(family = 'Helvetica', size = 48, weight = 'bold')
+
+#7 inch function
+def sevenProgram():
+  pass
+
+#10 inch function
+def tenProgram():
+  pass
+
+#12 inch function
+def twelveProgram():
+  pass
+
+#14 inch function
+def fourteenProgram():
+  pass
+
+#Stop function
+def stop():
+  pass
+
+#Button set up
+fourteenButton  = Button(screen, text = "14 inch", font = myFont, bg = "lightgreen", command = fourteenProgram, height = 4 , width = 8) 
+fourteenButton.place(x=610, y=0)
+
+twelveButton  = Button(screen, text = "12 inch", font = myFont, bg = "lightgreen", command = twelveProgram, height = 4 , width = 8) 
+twelveButton.place(x=405, y=0)
+
+tenButton  = Button(screen, text = "10 inch", font = myFont, bg = "lightgreen", command = tenProgram, height = 4 , width = 8) 
+tenButton.place(x=205, y=0)
+
+sevenButton  = Button(screen, text = "7 inch", font = myFont, bg = "lightgreen", command = sevenProgram, height = 4 , width = 8) 
+sevenButton.place(x=0, y=0)
+
+stopButton  = Button(screen, text = "STOP", font = myFontLarge, bg = "red", command = stop, height = 10 , width = 10) 
+stopButton.place(x=200, y=150)
+
+mainloop()
