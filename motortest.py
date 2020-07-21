feeb5fc7a6b6a1cf7e68855546cac358b1af77d5
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-21 14:44:34 -0400

    Just big motor

--- motortest.py
+++ motortest.py
@@ -8,7 +8,7 @@ GPIO.setwarnings(False)
 #Rotation and delay variables
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
-delay = .0125
+delay = .00005
 
 #Big stepper motor set up
 BIG_DIR = 21   # Direction GPIO Pin
@@ -25,22 +25,17 @@ SMALL_SPR = 200   # Steps per Revolution
 GPIO.setup(SMALL_DIR, GPIO.OUT)
 GPIO.setup(SMALL_STEP, GPIO.OUT)
 
-#delay =  -0.008*(i-.5)**4+.0005
-#delay = -0.0426667*(i**4)+0.0853333*(i**3)-0.0533333*(i**2)+0.0106667*i
-#delay = -4.2666666666666*(10**-14)*(i**4)+8.533333333333*(10**-11)*(i**3)-5.333333333333*(10**-8)*(i**2)+0.0000106667*i
-#delay = -2.6666666666666*(10**-15)*(i**4)+1.0666666666666*(10**-11)*(i**3)-1.333333333333*(10**-8)*(i**2)+5.333333333333*(10**-6)*i
 #Variables for steppers
-print("SMALL")
+print("BIG")
 
-#IN
-GPIO.output(SMALL_DIR, CCW)
-delay = .0005
-for i in range(2000):
-    delay = -2.6666666666666*(10**-15)*(i**4)+1.0666666666666*(10**-11)*(i**3)-1.333333333333*(10**-8)*(i**2)+5.333333333333*(10**-6)*i
-    print(delay)
-    GPIO.output(SMALL_STEP, GPIO.HIGH)
+#SPIN
+GPIO.output(BIG_DIR, CW)
+for i in range(1000):
+    GPIO.output(BIG_STEP, GPIO.HIGH)
     time.sleep(delay)
-    GPIO.output(SMALL_STEP, GPIO.LOW)
+    GPIO.output(BIG_STEP, GPIO.LOW)
     time.sleep(delay)
-    if(i == 1000):
-      GPIO.output(SMALL_DIR, CW)
+
+
+#Clean up pins
+GPIO.cleanup()
