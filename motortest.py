0518c33a5c3421b4af80e82f4fdfe6ee57101229
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-21 10:50:23 -0400

    Back to function

--- motortest.py
+++ motortest.py
@@ -20,55 +20,76 @@ GPIO.setup(BIG_STEP, GPIO.OUT)
 #Small stepper motor set up
 SMALL_DIR = 11   # Direction GPIO Pin
 SMALL_STEP = 12  # Step GPIO Pin
-SMALL_SPR = 200   # Steps per Revolution
 
 GPIO.setup(SMALL_DIR, GPIO.OUT)
 GPIO.setup(SMALL_STEP, GPIO.OUT)
 
-#Variables for steppers
-BIG_SPR = 200   # Steps per Revolution
-SPIN = 16 #Amount to spin for full revolution
-REPS = 4 #Number of times to run loop
+#Stepper motor variables
+straightAmt = 800
+spin = 16
 
 #DC motor set up
-rpwm = 5
-lpwm = 7
+in1 = 3
+in2 = 5
+en = 7
 
-GPIO.setup(rpwm,GPIO.OUT)
-GPIO.setup(lpwm,GPIO.OUT)
-GPIO.output(rpwm, GPIO.LOW)
-GPIO.output(lpwm,GPIO.LOW)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
 
-#Set lpwm high to go forward and rpwm high for backward...might only need one
-#since we just want one direction
-#Try creating GPIO.PWM(pin, 100) but not sure if it will work... p.start()
-#p.changeDutyCycle() and p.stop()
+#DC Motor commented out for testing of bottom motors
+#Create PWM instance with channel and 1000 frequency
+#*p=GPIO.PWM(en,1000)
+#Start with 25% duty cycle
+#*p.start(25)
+#The default speed & direction of motor is LOW & Forward....
+#SPEED
+    #p.ChangeDutyCycle(25 - low, 50 - med, 75 - high)
+#DIRECION
+    #GPIO.output(in1,GPIO.LOW)
+    #GPIO.output(in2,GPIO.LOW)
+    #forward: in1 HIGH, in2 LOW
+    #backward: in1 LOW, in2 HIGH
+    #stop: in1 LOW, in2 LOW
+#Stop PWM
+#*p.stop()
 
+GPIO.output(BIG_DIR, CW)
+GPIO.output(SMALL_DIR, CCW)
+wait = 5
 #Run small stepper to center
 print("Small step running inward")
 
-GPIO.output(SMALL_DIR, CCW)
-for x in range(REPS*SMALL_SPR):
-    GPIO.output(SMALL_STEP, GPIO.HIGH)
-    time.sleep(3*delay)
-    GPIO.output(SMALL_STEP, GPIO.LOW)
-    time.sleep(3*delay)
+for i in range(straightAmt*spin):
+    GPIO.output(BIG_STEP, GPIO.HIGH)
+    time.sleep(wait*delay)
+    GPIO.output(BIG_STEP, GPIO.LOW)
+    time.sleep(wait*delay)
+    if(i%(spin) == 0):
+      GPIO.output(SMALL_STEP, GPIO.HIGH)
+      time.sleep(wait*delay)
+      GPIO.output(SMALL_STEP, GPIO.LOW)
+      time.sleep(wait*delay)
+    wait = int((1.0/5184)((3.0*i/3200)-12)^4+1)
 
-#Run big and small stepper
+#Run big and small stepper no delay
 print("Big and small step running")
 
-GPIO.output(BIG_DIR, CW)
 GPIO.output(SMALL_DIR, CW)
-for i in range(int(REPS*SPIN*BIG_SPR)):
+wait = 1
+for i in range(straightAmt*spin):
     GPIO.output(BIG_STEP, GPIO.HIGH)
-    time.sleep(delay)
+    time.sleep(wait*delay)
     GPIO.output(BIG_STEP, GPIO.LOW)
-    time.sleep(delay)
-    if(i%(SPIN) == 0):
+    time.sleep(wait*delay)
+    if(i%(spin) == 0):
       GPIO.output(SMALL_STEP, GPIO.HIGH)
-      time.sleep(delay)
+      time.sleep(wait*delay)
       GPIO.output(SMALL_STEP, GPIO.LOW)
-      time.sleep(delay)
+      time.sleep(wait*delay)
+    wait = int((1.0/5184)((3.0*(12801-i)/3200)-12)^4+1)
 
 #Clean up pins
 GPIO.cleanup()
