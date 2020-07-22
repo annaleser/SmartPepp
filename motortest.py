349ba03be69815b3111eaf97851bfb3a5f12d9f8
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-22 15:57:27 -0400

    Added small motor

--- motortest.py
+++ motortest.py
@@ -17,25 +17,38 @@ BIG_STEP = 22  # Step GPIO Pin
 GPIO.setup(BIG_DIR, GPIO.OUT)
 GPIO.setup(BIG_STEP, GPIO.OUT)
 
-#Small stepper motor set up
-SMALL_DIR = 11   # Direction GPIO Pin
-SMALL_STEP = 12  # Step GPIO Pin
-SMALL_SPR = 200   # Steps per Revolution
+#DC Motor set up
+rpwm = 5
+lpwm = 7
 
-GPIO.setup(SMALL_DIR, GPIO.OUT)
-GPIO.setup(SMALL_STEP, GPIO.OUT)
+GPIO.setup(rpwm,GPIO.OUT)
+GPIO.setup(lpwm,GPIO.OUT)
+GPIO.output(rpwm, GPIO.LOW)
+GPIO.output(lpwm,GPIO.LOW)
+
+#DC Motor start
+p=GPIO.PWM(rpwm,50)
+p.stop()
+p.start(25)
 
 #Variables for steppers
 print("BIG")
 
 #SPIN
 GPIO.output(BIG_DIR, CW)
+GPIO.output(SMALL_DIR, CCW)
 for i in range(50000):
     GPIO.output(BIG_STEP, GPIO.HIGH)
-    time.sleep(delay)
+    time.sleep(.000025)
     GPIO.output(BIG_STEP, GPIO.LOW)
-    time.sleep(delay)
+    if(i%100 == 0){
+      GPIO.output(SMALL_STEP, GPIO.HIGH)
+      time.sleep(0.00025)
+      GPIO.output(SMALL_STEP, GPIO.LOW)
+      time.sleep(0.00025)
+    } else{
+      time.sleep(.000025)
+    }
 
-
-#Clean up pins
-GPIO.cleanup()
+#Stop motor
+p.stop()
