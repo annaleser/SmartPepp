26a9b683ab636bdd180f48ee9f8ca97b38c1aab1
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-23 12:15:30 -0400

    small test again

--- motortest.py
+++ motortest.py
@@ -8,7 +8,7 @@ GPIO.setwarnings(False)
 #Rotation and delay variables
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
-delay = .0001
+delay = .0125
 
 #Big stepper motor set up
 BIG_DIR = 21   # Direction GPIO Pin
@@ -20,27 +20,36 @@ GPIO.setup(BIG_STEP, GPIO.OUT)
 #Small stepper motor set up
 SMALL_DIR = 11   # Direction GPIO Pin
 SMALL_STEP = 12  # Step GPIO Pin
+SMALL_SPR = 200   # Steps per Revolution
 
 GPIO.setup(SMALL_DIR, GPIO.OUT)
 GPIO.setup(SMALL_STEP, GPIO.OUT)
 
-#DC Motor set up
-rpwm = 5
-lpwm = 7
-
-GPIO.setup(rpwm,GPIO.OUT)
-GPIO.setup(lpwm,GPIO.OUT)
-GPIO.output(rpwm, GPIO.LOW)
-GPIO.output(lpwm,GPIO.LOW)
-
+#delay =  -0.008*(i-.5)**4+.0005
+#delay = -0.0426667*(i**4)+0.0853333*(i**3)-0.0533333*(i**2)+0.0106667*i
+#delay = -4.2666666666666*(10**-14)*(i**4)+8.533333333333*(10**-11)*(i**3)-5.333333333333*(10**-8)*(i**2)+0.0000106667*i
+#delay = -2.6666666666666*(10**-15)*(i**4)+1.0666666666666*(10**-11)*(i**3)-1.333333333333*(10**-8)*(i**2)+5.333333333333*(10**-6)*i
+#delay = -4*(10**-15)*(i**4)+1.6*(10**-11)*(i**3)-2*(10**-8)*(i**2)+8*(10**-6)*i
 #Variables for steppers
-print("BIG")
+print("SMALL")
 
-#SPIN
-GPIO.output(BIG_DIR, CW)
+#IN
 GPIO.output(SMALL_DIR, CCW)
-while True:
-    GPIO.output(BIG_STEP, GPIO.HIGH)
+delay = .0005
+for i in range(2000):
+    delay = -2.6666666666666*(10**-15)*(i**4)+1.0666666666666*(10**-11)*(i**3)-1.333333333333*(10**-8)*(i**2)+5.333333333333*(10**-6)*i
+    print(delay)
+    GPIO.output(SMALL_STEP, GPIO.HIGH)
     time.sleep(delay)
-    GPIO.output(BIG_STEP, GPIO.LOW)
+    GPIO.output(SMALL_STEP, GPIO.LOW)
+    time.sleep(delay)
+    if(i == 1000):
+      GPIO.output(SMALL_DIR, CW)
+
+#OUT
+GPIO.output(SMALL_DIR, CW)
+for i in range(1000):
+    delay = -2.6666666666666*(10**-15)*((1000+i)**4)+1.0666666666666*(10**-11)*((1000+i)**3)-1.333333333333*(10**-8)*((1000+i)**2)+5.333333333333*(10**-6)*(1000+i)
+    time.sleep(delay)
+    GPIO.output(SMALL_STEP, GPIO.LOW)
     time.sleep(delay)
