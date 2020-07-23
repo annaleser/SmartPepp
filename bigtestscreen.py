ad073d375439fcf844ab16f64797be5a4c4a0396
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-23 11:29:55 -0400

    simplified

--- bigtestscreen.py
+++ bigtestscreen.py
@@ -40,29 +40,15 @@ screen.title("Big Spinner")
 myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
 myFontLarge = tkFont.Font(family = 'Helvetica', size = 80, weight = 'bold')
 
-def spin():
-  GPIO.output(BIG_STEP, GPIO.HIGH)
-  time.sleep(delay)
-  GPIO.output(BIG_STEP, GPIO.LOW)
-  time.sleep(delay)
-
-p = Process(target=spin)
-
-#spin function
 def spinProgram():
-  print("hi")
-  p.start()
-  p.join()
-
-#Stop function
-def stop():
-  p.terminate()
+  for i in range(10000):
+    GPIO.output(BIG_STEP, GPIO.HIGH)
+    time.sleep(delay)
+    GPIO.output(BIG_STEP, GPIO.LOW)
+    time.sleep(delay)
 
 #Button set up
-spinButton  = Button(screen, text = "Spin", font = myFont, bg = "lightgreen", command = spinProgram, height = 2 , width = 6) 
-spinButton.place(x=150, y=0)
-
-stopButton  = Button(screen, text = "STOP", font = myFontLarge, bg = "red", command = stop, height = 2 , width = 8) 
-stopButton.place(x=150, y=175)
+spinButton  = Button(screen, text = "SPIN", font = myFontLarge, bg = "blue", command = spinProgram, height = 2 , width = 8) 
+spinButton.place(x=150, y=175)
 
 mainloop()
