65f95cd5994674deab808654a6a4e018972dd866
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-27 12:54:04 -0400

    please work

--- testscreen.py
+++ testscreen.py
@@ -54,7 +54,7 @@ screen.title("Test Screen")
 
 #Fonts for screen
 myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
-myFontLarge = tkFont.Font(family = 'Helvetica', size = 70, weight = 'bold')
+myFontLarge = tkFont.Font(family = 'Helvetica', size = 64, weight = 'bold')
 
 #Functions for starting and stopping spin
 def spinProgram():
@@ -178,7 +178,7 @@ def stopAll():
     
 #Button set up
 stopButton  = Button(screen, text = "STOP", font = myFontLarge, bg = "red", command = stopAll, height = 2 , width = 4) 
-stopButton.place(x=150, y=100)
+stopButton.place(x=170, y=100)
 
 inButton  = Button(screen, text = "IN", font = myFont, bg = "green", command = inProgram, height = 2 , width = 4) 
 inButton.place(x=5, y=10)
@@ -188,17 +188,17 @@ outButton  = Button(screen, text = "OUT", font = myFont, bg = "purple", command 
 outButton.place(x=5, y=310)
 
 spinButton  = Button(screen, text = "SPIN", font = myFont, bg = "yellow", command = spinProgram, height = 2 , width = 4) 
-spinButton.place(x=450, y=100)
+spinButton.place(x=450, y=75)
 stopSpinButton  = Button(screen, text = "STOP", font = myFont, bg = "orange", command = stopSpinning, height = 2 , width = 4) 
-stopSpinButton.place(x=450, y=200)
+stopSpinButton.place(x=450, y=250)
 
 rpms = Text(screen, font = myFont, width=2, height=1)
-rpms.place(x=285, y=5)
+rpms.place(x=270, y=5)
 rpms.insert(END,"25")
 fasterButton = Button(screen, text = "<", font = myFont, bg = "pink", command = faster, height = 1 , width = 2)
-fasterButton.place(x=200, y=5)
+fasterButton.place(x=185, y=5)
 slowerButton = Button(screen, text = ">", font = myFont, bg = "grey", command = slower, height = 1 , width = 2)
-slowerButton.place(x=350, y=5)
+slowerButton.place(x=335, y=5)
 
 startSliceButton = Button(screen, text = "SLICE", font = myFont, bg = "aqua", command = sliceProgram, height = 1 , width = 4)
 startSliceButton.place(x=180, y=375)
