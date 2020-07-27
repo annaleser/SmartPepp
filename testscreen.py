2a7bc2ebc71615370001f12a8112b6b1b22bf1e4
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-27 11:22:41 -0400

    Even more adjustments

--- testscreen.py
+++ testscreen.py
@@ -124,15 +124,15 @@ def stopMoving():
 
 #Button set up
 spinButton  = Button(screen, text = "SPIN", font = myFontLarge, bg = "yellow", command = spinProgram, height = 2 , width = 5) 
-spinButton.place(x=100, y=0)
+spinButton.place(x=150, y=0)
 stopButton  = Button(screen, text = "STOP", font = myFontLarge, bg = "red", command = stopSpinning, height = 2 , width = 5) 
-stopButton.place(x=100, y=225)
+stopButton.place(x=150, y=225)
 
 inButton  = Button(screen, text = "IN", font = myFont, bg = "green", command = inProgram, height = 2 , width = 4) 
-inButton.place(x=1, y=0)
+inButton.place(x=1, y=25)
 stopButton  = Button(screen, text = "STOP", font = myFont, bg = "blue", command = stopMoving, height = 2 , width = 4) 
-stopButton.place(x=1, y=150)
+stopButton.place(x=1, y=175)
 outButton  = Button(screen, text = "OUT", font = myFont, bg = "purple", command = outProgram, height = 2 , width = 4) 
-outButton.place(x=1, y=300)
+outButton.place(x=1, y=325)
 
 mainloop()
