c06b4e99ab8537f8e04cc45550856a2618bf4ccb
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-08-14 12:51:26 -0400

    slightly slower to center because not moving

--- pepPlace.py
+++ pepPlace.py
@@ -107,7 +107,7 @@ def pepPizza(mSpin,bSpin,mMove,bMove,totalTime,reps):
   #Move slower at end test
   slice(20)
   spin(0,mSpin*totalTime+bSpin)
-  move(IN,mMove,bMove+3*totalTime/4*mMove-.00001)
+  move(IN,mMove,bMove+3*totalTime/4*mMove)
   time.sleep(totalTime/4)
   stopAll()
   back(reps)
@@ -172,9 +172,9 @@ def center():
   GPIO.output(SMALL_DIR, IN)
   for i in range(6000):
       GPIO.output(SMALL_STEP, GPIO.HIGH)
-      time.sleep(.00000075)
+      time.sleep(.000001)
       GPIO.output(SMALL_STEP, GPIO.LOW)
-      time.sleep(.00000075)
+      time.sleep(.000001)
   
   # Create start time var
   global startTime
@@ -185,9 +185,9 @@ def back(amt):
   GPIO.output(SMALL_DIR, OUT)
   for i in range(amt):
       GPIO.output(SMALL_STEP, GPIO.HIGH)
-      time.sleep(.00000075)
+      time.sleep(.000001)
       GPIO.output(SMALL_STEP, GPIO.LOW)
-      time.sleep(.00000075)
+      time.sleep(.000001)
 
 # Stop functions
 def stopSlicing():
